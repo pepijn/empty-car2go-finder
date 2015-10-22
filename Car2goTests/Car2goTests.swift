66e8a2f0d760fc902af20f4bcd5d40fe4aba534e
Pepijn Looije <p@epij.nl>
2015-10-22 22:09:40 +0200

    Initial Commit

---
+++ Car2goTests/Car2goTests.swift
@@ -0,0 +1,36 @@
+//
+//  Car2goTests.swift
+//  Car2goTests
+//
+//  Created by Pepijn Looije on 22/10/15.
+//  Copyright © 2015 Pepijn Looije. All rights reserved.
+//
+
+import XCTest
+@testable import Car2go
+
+class Car2goTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
