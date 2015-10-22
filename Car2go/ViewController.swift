9cb4c837af4d6d93739231ee7edffbb206071533
Pepijn Looije <p@epij.nl>
2015-10-22 23:29:41 +0200

    Initial commit

--- Car2go/ViewController.swift
+++ Car2go/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let center = CLLocationCoordinate2D.init(latitude: 52.37, longitude: 4.99)
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.22, longitudeDelta: 0.4)
+        map.setRegion(MKCoordinateRegion.init(center: center, span: span), animated: true)
+
+        let annotation = MKPointAnnotation()
+
+        map.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        print(mapView.region)
+    }
 
 
 }
-
---
+++ README.md
@@ -0,0 +1,4 @@
+Car2go empty cars mobile
+------------------------
+
+https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/LocationAwarenessPG/Introduction/Introduction.html
